76a92c75aa232861c0856a1b3a0b62bfec2357bc
zerone2 <yicho93@naver.com>
2018-05-23 18:05:27 +0900

    js update

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+var btn = document.getElementById('selectBtn');
+
+function selClick(){
+
+	var selected_value = document.getElementById("selBox").value;
+
+	if(selected_value == "weather"){
+		location.href = "weather.html";
+	} else if(selected_value == "searchPlace"){
+		location.href = "searchPlace.html";
+	} else if(selected_value == "currentLoc"){
+		location.href = "currentPlace.html";
+	}
+
+}
